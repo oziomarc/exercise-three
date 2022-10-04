a55c9e0438d130889bf7342a6962445a52885d81
oziomarc <oziomarc@gmail.com>
2022-10-04 09:43:20 -0400

    got data

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { WEATHER_APP_API_KEY } from "../API_KEYS";
+import axios from "axios";
+
+    // query openapi for weather data
+    // make request
+    const openWeatherURL = `https://api.openweathermap.org/data/2.5/weather?q=Baltimore&appid=${WEATHER_APP_API_KEY}`;
 
 function Home() {
-    return(
+
+    // value stored in state for weather data
+    const [weatherData, setWeatherData] = useState({});
+
+    useEffect(() => {
+        axios
+        .get(openWeatherURL)
+        .then(function (response) {
+        // handle success
+            setWeatherData(response.data)
+        })
+        .catch(function (error) {
+            // handle error
+            console.warn(error);
+            setWeatherData({})
+        });
+    }, [])
+    console.log("state value", weatherData)
+
+    return (
         <div>
             <h1>Weather App</h1>
-            <p>{WEATHER_APP_API_KEY}</p>
+            <h2>City: {weatherData.name}</h2>
+            <p>Humidity: {weatherData.main.humidity}%</p>
         </div>
     )
 }
